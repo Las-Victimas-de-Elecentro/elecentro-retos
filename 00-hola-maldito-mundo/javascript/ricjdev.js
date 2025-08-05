fe587d25edcfb39ef6b61ed5564327718b1b568b
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-08-05 16:09:57 -0400

    01 - javascript
    primera solucion escrita en JS

---
+++ 00-hola-maldito-mundo/javascript/ricjdev.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+// comentario en una linea
+
+/*
+  Comentario pendejo pero en varias lineas
+*/
+
+let myAge = 22
+
+const myName = 'Ricardo'
+const myStr = 'Esta es una string'
+const myInt = 3
+const myFloat = 3.45
+const myBool = true
+const myUndefined = undefined
+
+console.log('Hola, JavaScript')
+console.log(`Tengo ${myAge} años`)
+console.log(`Mi nombre es ${myName}`)
+console.log(`${myFloat} es un number`)
